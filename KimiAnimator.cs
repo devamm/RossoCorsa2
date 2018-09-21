510d69bf5bb555fbca4a8a645bdfd50283badfe1
Devam <devam.malaviya@gmail.com>
2018-09-21 14:36:38 -0400

    initial commit

---
+++ KimiAnimator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KimiAnimator : MonoBehaviour {
+	Transform kimi;
+
+	public float speed;
+
+	void Start () {
+		kimi = GameObject.Find("kimi0").GetComponent<Transform> ();
+
+	}
+
+	void Update () {
+		if(Input.GetKey(KeyCode.W)){
+			kimi.Translate(Vector3.up * speed);
+		}
+		
+		else if(Input.GetKey(KeyCode.S)){
+			kimi.Translate(-Vector3.up * speed);
+		}
+
+		else if(Input.GetKey(KeyCode.D)){
+			kimi.Translate(Vector3.right * speed);
+		}
+
+		else if(Input.GetKey(KeyCode.A)){
+			kimi.Translate(-Vector3.right * speed);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D obj){
+		if(obj.gameObject.tag != "wall"){
+			Debug.Log("Collision!", null);
+		}
+	}
+}
---
+++ enemyScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour {
+	public Transform rep1;
+	public float moveSpeed;
+	public float moveTime;
+	public float interval;
+
+	private float moveCounter;
+	private float intervalCounter;
+	private bool moving = true;
+	private float direction = -1f;
+
+	// Use this for initialization
+	void Start () {
+		rep1 = GameObject.Find("reporter1").GetComponent<Transform>();	
+		intervalCounter = interval;
+		moveCounter = moveTime;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(moving){
+			moveCounter -= Time.deltaTime;
+		
+			if (direction < 0){
+				rep1.Translate(Vector3.right * moveSpeed);
+			} else {
+				rep1.Translate(-Vector3.right * moveSpeed);
+			}
+			
+			if(moveCounter < 0){
+				moving = false;
+				intervalCounter = interval;
+				direction = Random.Range(-1f, 1f);
+			}
+		} else {
+			intervalCounter -=Time.deltaTime;
+			if(intervalCounter < 0){
+				moving = true;
+				moveCounter = moveTime;
+				direction = Random.Range(-1f, 1f);
+			
+			}
+		}
+	}
+	void OnCollisionEnter2D(Collision2D obj){
+		if(obj.gameObject.tag == "wall"){
+			direction *= -1;
+		}
+	}
+}
