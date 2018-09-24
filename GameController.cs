4390e1dbdc3f524597189c513a4c264d55b516ac
Devam <devam493@gmail.com>
2018-09-24 11:49:14 -0400

    split menu and game controller into separate files

--- GameController.cs
+++ GameController.cs
@@ -5,45 +5,26 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour {
 
 	private Animator anim;
-	private Renderer menuRender;
 	private bool win;
 	private bool gameOver;
-	private bool menu;
 
 
 	// Use this for initialization
 	void Start () {
-		menu = true;
-		anim = null;
-		DontDestroyOnLoad(this);
-		menuRender = GetComponent<Renderer>();
+		anim = GameObject.Find("kimi0").GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {		
-		if(menu){
-			menuRender.enabled = true;
-			if(Input.GetKeyDown(KeyCode.Space)){
-				menu = false;
-				SceneManager.LoadScene("MainGame");
-				
-			}
-		} else {
-			menuRender.enabled = false;
-			anim = GameObject.Find("kimi0").GetComponent<Animator>();
-			win = anim.GetBool("Win");
-			gameOver = anim.GetBool("GameOver");
-			
-			if(win){
-				Debug.Log("You won!", null);
-				menu = true;
-				SceneManager.LoadScene("Menu");
-			} 
-			if(gameOver){
-			Debug.Log("Game over", null);
+		Debug.Log("game running", null);
+		win = anim.GetBool("Win");
+		gameOver = anim.GetBool("GameOver");
+
+		if(gameOver){
 			SceneManager.LoadScene("MainGame");
-			
-			}
 		}
+		if(win){
+			SceneManager.LoadScene("Menu");
+		}	
 	}
 }
